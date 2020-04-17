1d032c80b0c6c8e3f3b34af0fe5b5e507bcebcf8
Tim-Abwao <abwaomusungu@gmail.com>
2020-04-17 21:56:51 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, flash, render_template, url_for, request, redirect
+from ts_functions import get_graphs, clear_old_files
+import os
+from werkzeug.utils import secure_filename
+import pandas as pd
+import numpy as np
+
+
+UPLOAD_FOLDER = 'static/files'
+ALLOWED_EXTENSIONS = {'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', start=True)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            clear_old_files('csv')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('process_file',
+                                    filename=filename))
+    return render_template('upload.html')
+
+
+@app.route('/processing_<filename>')
+def process_file(filename):
+    ts = pd.read_csv('static/files/' + filename, index_col=0)
+    ts.index = pd.to_datetime(ts.index)
+    graphs = get_graphs(ts)
+
+    return render_template('processing_file.html', graphs=graphs,
+                           filename=filename)
+
+
+@app.route('/sample', methods=['GET', 'POST'])
+def create_sample():
+    if request.method == 'GET':
+        return render_template('processing_sample.html', sample=True)
+    if request.method == 'POST':
+        size = int(request.form['size'])
+        ts = pd.Series(np.random.rand(size),
+                       index=pd.date_range('2020-01-01', periods=size))
+        graphs = get_graphs(ts)
+        return render_template('processing_sample.html', graphs=graphs)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ ts_functions.py
@@ -0,0 +1,30 @@
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
+import matplotlib.pyplot as plt
+from datetime import datetime
+import glob
+import os
+
+
+def clear_old_files(extension):
+    old_files = glob.glob('static/files/*.' + extension, recursive=True)
+    for file in old_files:
+        os.remove(file)
+
+
+def get_graphs(data):
+    clear_old_files('png')
+    t = str(datetime.now())
+    graphs = ['static/files/' + t+i for i in ['_acf_pacf_plots.png',
+                                              '_plot.png']]
+    plt.figure(figsize=(10, 6))
+    plt.plot(data)
+    plt.savefig(graphs[0])
+
+    fig1 = plt.figure(figsize=(10, 8))
+    ax1 = fig1.add_subplot(211)
+    plot_acf(data.values, ax=ax1, color='coral')
+    ax2 = fig1.add_subplot(212)
+    plot_pacf(data.values, ax=ax2, color='coral')
+    plt.savefig(graphs[1])
+
+    return graphs
