bbb0b382500b5442ba695e14d2ebfd42528f1679
Tim-Abwao <abwaomusungu@gmail.com>
2020-05-11 12:25:56 +0300

    Updating README.md

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 A simple web app to learn a little about Time Series analysis and forecasting.
 
 *\*Still under development\**.
-To give it a try, [click here...](time-series-app.herokuapp.com)
+To give it a try, [click here...](https://time-series-app.herokuapp.com)
 
 ## Running locally
 
 ### Prerequisites
 
-[Python](https://www.python.org).
+[Python](https://www.python.org). Packages used include [statsmodels](https://www.statsmodels.org/stable/index.html), [flask](https://flask.palletsprojects.com/en/1.1.x/), [pandas](https://pandas.pydata.org) and [NumPy](https://numpy.org).
 
 ### Getting started
 
--- ts_functions.py
+++ ts_functions.py
@@ -7,6 +7,40 @@ import glob
 import os
 
 
+def fit_tsmodels(data):
+    """
+    This function fits AR, SARIMAX and Holt-Winters Exponential Smoothing
+    models on the {data}. It then makes predictions over 60% of the data,
+    plots graphs to show model-fit, and saves them as png files. FInally, a
+    dataframe of sample results, and list of graph locations are returned.
+    """
+    # to predict over 60% of the data
+    start = data.index[int(len(data) * 0.4)]
+    end = data.index[-1]
+    pred1 = sm.tsa.AutoReg(data, lags=10).fit().predict(start, end)
+    pred2 = sm.tsa.SARIMAX(data).fit().predict(start, end)
+    pred3 = sm.tsa.ExponentialSmoothing(data).fit().predict(start, end)
+    # getting sample results as a dataframe
+    models_dict = {"AR": pred1, "SARIMAX": pred2,
+                   "Exponential Smoothing": pred3}
+    df = pd.DataFrame({**models_dict})
+    results = pd.concat([data, df], axis=1).tail(14).round(2)
+    # plotting results
+    graphs_names = []
+    t = str(datetime.now())
+    for model, values in models_dict.items():
+        plt.figure(figsize=(8, 4.5))
+        plt.plot(data, label="Original", color="navy")
+        plt.plot(values, label="Modelled", color="aqua")
+        plt.legend()
+        plt.title(model + " Model Fit", size=15, pad=10)
+        plt.xticks(rotation=90)
+        name = "static/files/" + "_".join([t, model]) + ".png"
+        graphs_names.append(name)
+        plt.savefig(name, transparent=True)
+    return results, graphs_names
+
+
 def clear_old_files(extension, filepath="static/files/*."):
     """
     A utility function to remove old files of {extension} format, from the
@@ -41,38 +75,3 @@ def get_graphs(data):
     plot_pacf(data.values, ax=ax2, color="navy")
     plt.savefig(graphs_names[1], transparent=True)
     return graphs_names
-
-
-def fit_tsmodels(data):
-    """
-    This function fits AR, SARIMAX and Holt-Winters Exponential Smoothing
-    models on the {data}. It then makes predictions over 60% of the data,
-    plots graphs to show model-fit, and saves them as png files. FInally, a
-    dataframe of sample results, and list of graph locations are returned.
-
-    """
-    # to predict over 60% of the data
-    start = data.index[int(len(data) * 0.4)]
-    end = data.index[-1]
-    pred1 = sm.tsa.AutoReg(data, lags=10).fit().predict(start, end)
-    pred2 = sm.tsa.SARIMAX(data).fit().predict(start, end)
-    pred3 = sm.tsa.ExponentialSmoothing(data).fit().predict(start, end)
-    # getting sample results as a dataframe
-    models_dict = {"AR": pred1, "SARIMAX": pred2,
-                   "Exponential Smoothing": pred3}
-    df = pd.DataFrame({**models_dict})
-    results = pd.concat([data, df], axis=1).tail(14).round(2)
-    # plotting results
-    graphs_names = []
-    t = str(datetime.now())
-    for model, values in models_dict.items():
-        plt.figure(figsize=(8, 4.5))
-        plt.plot(data, label="Original", color="navy")
-        plt.plot(values, label="Modelled", color="aqua")
-        plt.legend()
-        plt.title(model + " Model Fit", size=15, pad=10)
-        plt.xticks(rotation=90)
-        name = "static/files/" + "_".join([t, model]) + ".png"
-        graphs_names.append(name)
-        plt.savefig(name, transparent=True)
-    return results, graphs_names
