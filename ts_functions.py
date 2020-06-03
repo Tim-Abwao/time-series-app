d157c12ebd5aba287019530df976f2466379e3fd
Tim-Abwao <abwaomusungu@gmail.com>
2020-06-03 22:50:05 +0300

    Improving the graphical output

--- ts_functions.py
+++ ts_functions.py
@@ -3,11 +3,13 @@ from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from datetime import datetime
-import glob
+from glob import glob
 import os
 import matplotlib
 
-matplotlib.use("Agg")
+matplotlib.rcParams["figure.autolayout"] = True
+matplotlib.rcParams["backend"] = "Agg"
+matplotlib.rcParams["legend.frameon"] = True
 
 
 def clear_old_files(extension, filepath="static/files/*."):
@@ -15,7 +17,7 @@ def clear_old_files(extension, filepath="static/files/*."):
     A utility function to remove old files of {extension} format, from the
     {filepath} folder.
     """
-    old_files = glob.glob(filepath + extension, recursive=True)
+    old_files = glob(filepath + extension, recursive=True)
     for file in old_files:
         os.remove(file)
 
@@ -46,7 +48,12 @@ class TimeSeriesPredictions:
         models_dict = {"AR": model1, "Exponential Smoothing": model2}
         return pd.DataFrame({**models_dict})
 
-    def sample(self, data, size=14):
+    def sample(self, data, size=14) -> pd.DataFrame:
+        """
+        Creates a sample, a dataframe with the original and modelled data.
+        This will be displayed as the table in the results page.
+        """
+
         return pd.concat([data, self.results], axis=1).round(2).tail(size)
 
 
@@ -62,7 +69,7 @@ class TimeSeriesGraphs:
     def plot_acf_pacf(self, data):
         """
         Produces the ACF & PACF graph, saving it as a png file, and returning
-        it's path.
+        it's location(name).
         """
         fig = plt.figure(figsize=(8, 6))
         ax1 = fig.add_subplot(211)
@@ -78,13 +85,12 @@ class TimeSeriesGraphs:
 
     def plot_line(self, data):
         """
-        Plots the data, saves it as a png image, and returns the save name.
+        Plots the data, saves it as a png file, and returns the file location.
         """
         plt.figure(figsize=(8, 4.5))
         plt.plot(data, color="navy")
-        plt.xticks(rotation=90)
+        plt.xticks(rotation=30)
         plt.title("A line-plot of the data", size=15, pad=10)
-
         name = self.file_folder + str(datetime.now()) + "_line_plot.png"
         plt.savefig(name, transparent=True)
 
@@ -93,17 +99,16 @@ class TimeSeriesGraphs:
     def plot_model_fit(self, data, results):
         """
         Plots the original and predicted values for each time series model
-        fitted, saves the graphs as images, and returns a list of graph
-        paths.
+        fitted, saves the graphs as png files, and returns a list of file
+        locations(names).
         """
         names = []
         for model, values in results.iteritems():
             plt.figure(figsize=(8, 4.5))
             plt.plot(data, label="Original", color="navy")
             plt.plot(values, label="Modelled", color="aqua")
-            plt.legend()
             plt.title(model + " Model Fit", size=15, pad=10)
-            plt.xticks(rotation=90)
+            plt.xticks(rotation=30)
             name = self.file_folder + str(datetime.now()) + "-" + model + ".png"
             names.append(name)
             plt.savefig(name, transparent=True)
@@ -111,10 +116,12 @@ class TimeSeriesGraphs:
         return names
 
     def plot_seanonal_decomposition(self, data):
-        name = (
-            self.file_folder + str(datetime.now()) + "_seasonal-decomposition.png"
-        )
-        sm.tsa.seasonal_decompose(data).plot()
+        """
+        Performs basic seasonal decomposition, plots the various components,
+        saves them as a png file, and returns their location.
+        """
+        name = self.file_folder + str(datetime.now()) + "_seasonal-decomposition.png"
+        sm.tsa.seasonal_decompose(data).plot().autofmt_xdate()
         plt.savefig(name, transparent=True)
 
         return name
