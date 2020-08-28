6afe6322840b3d439e9ca78795a5edeff54ebecd
Tim-Abwao <abwaomusungu@gmail.com>
2020-08-29 00:17:27 +0300

    Improving code readebility

--- app.py
+++ app.py
@@ -18,106 +18,109 @@ glossary_data.reset_index(drop=True, inplace=True)
 
 def allowed_file(filename):
     """
-    Checks whether a file's extension is supported/allowed.
+    Check whether upload-file's extension is allowed.
     """
     return "." in filename and filename.rsplit(".", 1)[1].lower() in {"csv"}
 
 
 def get_ts_results(data):
     """
-    A convenience funtion to nicely consolidate the output/results
+    Parse results as a dict to pass to display in the html templates.
     """
-    ts_results = {}
-    clear_files('png')
-    # fitting the time series models
-    results_instance = TimeSeriesResults(data)
-    ts_results['results'] = results = results_instance.results
-    ts_results['sample'] = sample = results_instance.sample
-    ts_results['totals'] = sample.sum().round(2).to_numpy()
-    ts_results['graphs'] = {
-        "acf&pacf": results_instance.acf_pacf,
-        "lineplot": results_instance.lineplot,
-        "model_fit": results_instance.modelfit,
-        "seasonal_decomposition": results_instance.seasonal_decomposition
+    clear_files('svg')
+
+    result = TimeSeriesResults(data)
+    ts_results = {
+        'results': result.predictions,
+        'sample': result.predictions.tail(14),
+        'graphs': {
+            "acf&pacf": result.acf_pacf,
+            "lineplot": result.lineplot,
+            "model_fit": result.modelfit,
+            "seasonal_decomposition": result.seasonal_decomposition
+        }
     }
-    results.to_csv('static/results.csv')
+    ts_results['totals'] = ts_results['sample'].sum().to_numpy(),
+    ts_results['results'].to_csv('static/results.csv')
     return ts_results
 
 
 @app.route("/")
 def index():
+    """Home page."""
     return render_template("index.html")
 
 
 @app.route("/glossary")
 def glossary():
+    """Glossary page."""
     return render_template("glossary.html", definitions=glossary_data)
 
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload_file():
+    """Process and save uploaded file."""
     if request.method == "POST":
-        # selecting the file part
         try:
             file = request.files["file"]
         except KeyError:
             return redirect(request.url)
 
-        # Ensuring the file exists (some browsers send an empty file part if
-        # no file is selected).
-        if file.filename == "":
+        if file.filename == "":  # if file not suplied to input form
             return redirect(request.url)
 
-        # processing the file
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            clear_files("csv")  # removing outdated uploads
+            clear_files("csv")  # remove all previous .csv uploads
+
+            # Parse file as a pandas dataframe
             try:
                 data = pd.read_csv(file, index_col=0)
-            except (pdParserError, UnicodeDecodeError):
-                # raised if the file isn't CSV
-                input_error = "Please try again... the uploaded file is not" +\
-                    " in standard CSV format."
+                if (n := len(data)) < 30:
+                    input_error = f"""Please try again... The uploaded file has
+                        only {n} values, but the minimum is set at 30."""
+                    return render_template("upload.html",
+                                           input_error=input_error)
+                if data.shape[1] < 1:
+                    input_error = """Please ensure that the data has a date
+                        column and at least one other column with numeric
+                        values."""
+                    return render_template("upload.html",
+                                           input_error=input_error)
+                data = data.iloc[:, -1]  # select last column for analysis
+
+                try:
+                    data = data.astype(float)
+                except ValueError:
+                    input_error = """Please try again... it seems the values
+                        to be processed could not be converted to numbers."""
+                    return render_template("upload.html",
+                                           input_error=input_error)
+                # Convert the index to datetime data type
+                try:
+                    data.index = pd.to_datetime(data.index)
+                except dtParserError:
+                    input_error = """Please try again... it seems that the
+                        values in the 1st column could not be read as dates."""
+                    return render_template("upload.html",
+                                           input_error=input_error)
+
+                # If date frequency can't be inferred, some statsmodels
+                # functions (e.g. seasonal_decompose) tend to break.
+                if pd.infer_freq(data.index) is None:
+                    input_error = """Please try again... a uniform date
+                        frequency which is needed in some of the time series
+                        functions used) could not be determined."""
+                    return render_template("upload.html",
+                                           input_error=input_error)
+
+                data.to_csv("static/files/" + filename)
+
+            except pdParserError:
+                input_error = """Please try again... the uploaded file could
+                    not be parsed as a CSV file."""
                 return render_template("upload.html", input_error=input_error)
 
-            try:
-                data.index = pd.to_datetime(data.index)
-            except (dtParserError, TypeError):
-                # raised if index can't be read as dates
-                input_error = "Please try again... it seems that the values" +\
-                    " in the 1st column could not be read as dates."
-                return render_template("upload.html", input_error=input_error)
-
-            if (n := len(data)) < 30:  # avoiding errors due to small samples
-                # String concatenation is used to minimize the tab characters
-                # (indentation) that appear in the generated html text.
-                input_error = "Please try again... The uploaded file has " +\
-                 f"only {n} values, but the minimum is set at 30."
-                return render_template("upload.html", input_error=input_error)
-
-            # Handling cases where date frequency can't be inferred, which
-            # breaks some statsmodels functions (e.g. seasonal_decompose)
-            if pd.infer_freq(data.index) is None:
-                input_error = "Please try again... a uniform date frequency" +\
-                 "(which is needed in some of the time series functions used)"\
-                 + "could not be determined."
-                return render_template("upload.html", input_error=input_error)
-
-            if data.shape[1] < 1:
-                input_error = "Please ensure that the data has a date column\
-                              and at least one other column."
-                return render_template("upload.html", input_error=input_error)
-
-            # saving the file, if it is valid
-            data = data.iloc[:, -1]  # selecting last column for analysis
-
-            try:
-                data = data.astype('float32')
-            except ValueError:
-                input_error = "Please try again... it seems the values to be"\
-                   + " processed could not be converted to numbers."
-                return render_template("upload.html", input_error=input_error)
-            data.to_csv("static/files/" + filename)
             return redirect(url_for("process_file", filename=filename))
 
     return render_template("upload.html")
@@ -125,25 +128,22 @@ def upload_file():
 
 @app.route("/processing_<filename>", methods=["GET", "POST"])
 def process_file(filename):
-    # importing data from uploaded file
+    """
+    Analyse uploaded data, and transmit results to html template.
+    """
     try:
         data = pd.read_csv("static/files/" + filename, index_col=0)
-        data = data.iloc[:, -1]  # selecting last column for analysis
         data.index = pd.to_datetime(data.index)
-    except (EmptyDataError, FileNotFoundError):  # raised if file wasn't saved
-        input_error = "Please try uploading the file again."
+    except (EmptyDataError, FileNotFoundError):
+        input_error = "Please try uploading the file once again."
         return render_template("upload.html", input_error=input_error)
-
-    return render_template(
-        "processing_file.html",
-        filename=filename,
-        **get_ts_results(data)
-    )
+    return render_template("processing_file.html", filename=filename,
+                           **get_ts_results(data))
 
 
 @app.route("/sample", methods=["GET", "POST"])
 def create_sample():
-
+    """Create and process sample time series data."""
     sample_params = {'start': date.today().isoformat(),
                      'end': (date.today() + timedelta(days=30)).isoformat(),
                      'frequencies': {"D": "Days",
@@ -157,7 +157,6 @@ def create_sample():
 
     if request.method == "POST":
         try:
-            # collecting parameters from form
             start = request.form["start_date"]
             stop = request.form["end_date"]
             frequency = request.form["frequency"]
@@ -168,52 +167,35 @@ def create_sample():
             frequency, ar_order, ma_order = "D", 1, 1
 
         index = pd.date_range(start, stop, freq=frequency)
-        size = len(index)
+        if n := len(index) < 30:
+            input_error = f"""Please try again... The generated sample has
+                {n} value(s) ({sample_params['frequencies'][frequency]})
+                between {start} and {stop}, but the minimum sample size is set
+                at 30."""
+            return render_template("processing_sample.html",
+                                   sample_params=sample_params,
+                                   input_error=input_error)
 
-        # Limit sample size to >= 30
-        if size < 30:
-            input_error = f"""
-            Please try again... The generated sample has {size} value(s)
-            ({sample_params['frequencies'][frequency]}) between {start} and
-            {stop}, but the minimum sample size is set at 30."""
-            return render_template(
-                "processing_sample.html",
-                sample_params=sample_params,
-                input_error=input_error,
-            )
-
-        # creating user-defined ARMA sample
         np.random.seed(123)
         ar = np.linspace(-0.9, 0.9, ar_order)
         ma = np.linspace(-1, 1, ma_order)
         arma_sample = arma_generate_sample(
-            ar, ma, size, scale=100, distrvs=np.random.standard_normal
+            ar, ma, len(index), scale=100, distrvs=np.random.standard_normal
         )
         data = pd.Series(arma_sample, index=index, name="Sample")
 
-        return render_template(
-            "processing_file.html",
-            filename="Sample",
-            **get_ts_results(data)
-        )
+        return render_template("processing_file.html", filename="Sample",
+                               **get_ts_results(data))
 
-        return render_template(
-            "processing_sample.html",
-            sample_params=sample_params,
-            input_error=input_error,
-        )
-
-    return render_template(
-        "processing_sample.html",
-        sample_params=sample_params
-    )
+    return render_template("processing_sample.html",
+                           sample_params=sample_params)
 
 
 @app.route("/server_timeout")
 def heroku_timeout():
     """
-    This creates the custom error page to be displayed if the app takes more
-    than 30 seconds (the limit at Heroku platform) to process a request.
+    Create the custom error page to be displayed if the app takes more
+    than 30 seconds to process a request.
     See https://devcenter.heroku.com/articles/error-codes#h12-request-timeout
     """
     return render_template('heroku_custom_503.htm')
--- fit_time_series.py
+++ fit_time_series.py
@@ -15,122 +15,128 @@ plt = matplotlib.pyplot
 
 def clear_files(extension, filepath="static/files/"):
     """
-    Deletes files of {extension} format, from the {filepath} directory.
+    Delete files of specified format from the supplied directory.
     """
-    [os.remove(file) for file in glob("".join([filepath, "*.", extension]),
-                                      recursive=True)]
+    files = glob("".join([filepath, "*.", extension]), recursive=True)
+    [os.remove(file) for file in files]
 
 
 class TimeSeriesPredictions:
-    """A class of methods for fitting time series models"""
+    """
+    Fit various time series models and make predictions, using `statsmodels`.
+    """
 
-    def _prediction_scope(self, data, coverage=0.6):
-        """
-        Returns the prediction start & end dates that cover {coverage} * 100%
-        of the data (60% by default).
-        """
-        n_rows, index = len(data), data.index
-        start = index[int((1 - coverage) * n_rows)]
-        end = index[-1]
-        return start, end
+    def __init__(self, data):
+        self.data = data
+        self._prediction_scope()
+        self._fit_ts_models()
 
-    def _fit_ts_models(self, data):
+    def _prediction_scope(self, coverage=0.6):
         """
-        Fits various time series models on the data, and returns their
-        predictions as a dataframe.
+        Get the time-range so that predictions span `coverage%`(60% default)
+        of the data.
         """
-        scope = self._prediction_scope(data)
+        start = self.data.index[int((1 - coverage) * len(self.data))]
+        end = self.data.index[-1]
+        self.prediction_range = start, end
+
+    def _fit_ts_models(self):
+        """
+        Fit time series models on the data, and get predictions as a dataframe.
+        """
+        scope = self.prediction_range
         ts_predictions = {
-            "Exponential Smoothing": sm.tsa.ExponentialSmoothing(
-                data, trend="add", seasonal="add").fit().predict(*scope),
-            "AR": sm.tsa.AutoReg(data, lags=10).fit().predict(*scope)
+            "Exponential Smoothing": (sm.tsa.ExponentialSmoothing(
+                                        self.data, trend="add", seasonal="add"
+                                        ).fit()
+                                         .predict(*scope)),
+            "AR": (sm.tsa.AutoReg(self.data, lags=10)
+                     .fit()
+                     .predict(*scope))
             }
-        # Searching for an appropriate ARMA order
+        # Searching for appropriate ARMA order
         try:
-            arma_ic = sm.tsa.arma_order_select_ic(
-                data, ic="bic", trend="nc", fit_kw={"dist": 0}
-            )
+            arma_ic = sm.tsa.arma_order_select_ic(self.data, ic="bic",
+                                                  trend="nc",
+                                                  fit_kw={"dist": 0})
+            # Set ARMA order order >= (1, 1)
             arma_order = max(arma_ic.bic_min_order, (1, 1))
-            ts_predictions['ARMA'] = sm.tsa.ARMA(data, arma_order).fit(disp=0)\
-                                       .predict(*scope)
+            ts_predictions['ARMA'] = (sm.tsa.ARMA(self.data, arma_order)
+                                        .fit(disp=0)
+                                        .predict(*scope))
         except ValueError:  # raised if above arma model fails to converge
-            # Explicitly attempt to fit an ARMA (1,1) model
-            ts_predictions['ARMA'] = sm.tsa.ARMA(data, (1, 1)).fit(disp=0)\
-                                       .predict(*scope)
+            ts_predictions['ARMA'] = (sm.tsa.ARMA(self.data, (1, 1))
+                                        .fit(disp=0)
+                                        .predict(*scope))
 
-        return pd.DataFrame({'Actual Data': data, **ts_predictions}).dropna()
+        self.predictions = pd.DataFrame({'Actual Data': self.data,
+                                         **ts_predictions}).dropna()
 
 
 class TimeSeriesResults(TimeSeriesPredictions):
     """
-    The class of methods to compute and store time series forecasting results
+    Plot graphs to asses time series predictions.
     """
 
     def __init__(self, data):
-        self.results = super()._fit_ts_models(data).round(2)
-        self.sample = self.results.tail(14)
-        self.file_folder = "static/files/"
-        self.acf_pacf = self._plot_acf_pacf(data)
-        self.lineplot = self._plot_line(data)
-        self.modelfit = self._plot_model_fit(data, self.results)
-        self.seasonal_decomposition = self._plot_seanonal_decomposition(data)
+        super().__init__(data)
+        self.save_dir = "static/files/"
+        self._plot_acf_pacf()
+        self._plot_line()
+        self._plot_model_fit()
+        self._plot_seanonal_decomposition()
         self._terminate()
 
-    def _plot_acf_pacf(self, data):
+    def _plot_acf_pacf(self):
         """
-        Produces the ACF & PACF graphs, saving them as a png file, and
-        returning their location.
+        Plot ACF & PACF graphs of the data, and save them as an svg file.
         """
         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
-        plot_acf(data.values, ax=ax1, color="navy")
-        plot_pacf(data.values, ax=ax2, color="navy")
-        return self._save_graph("acf_pacf_plots.png")
+        plot_acf(self.data.values, ax=ax1, color="navy")
+        plot_pacf(self.data.values, ax=ax2, color="navy")
+        self.acf_pacf = self._save_graph("acf_pacf_plots.svg")
 
-    def _plot_line(self, data):
+    def _plot_line(self):
         """
-        Plots the data, saves the graph as a png file, and returns its
-        location.
+        Plot the data, and save the graph as an svg file.
         """
         plt.figure(figsize=(8, 4))
-        plt.plot(data, color="navy")
+        plt.plot(self.data, color="navy")
         plt.xticks(rotation=30)
         plt.title("A line-plot of the data", size=15, pad=10)
-        return self._save_graph("line_plot.png")
+        self.lineplot = self._save_graph("line_plot.svg")
 
-    def _plot_model_fit(self, data, results):
+    def _plot_model_fit(self):
         """
-        Plots the original and predicted values for each time series model
-        fitted, saves the graphs as a png file, and returns its location.
+        Plot the original and predicted values for each time series model
+        fitted, and save the graph as an svg file.
         """
-        fig = plt.figure(figsize=(8, 16))
-        idx = 1
-        for model, values in results.drop('Actual Data', axis=1).iteritems():
-            fig.add_subplot(3, 1, idx)
-            plt.plot(data, label="Original", color="navy")
-            plt.plot(values, label="Modelled", color="aqua")
-            plt.title(model + " Model Fit", size=15, pad=15)
-            plt.xticks(rotation=30)
-            plt.legend()
-            idx += 1
+        fig, axs = plt.subplots(3, 1, figsize=(8, 16))
+        predictions = self.predictions.drop('Actual Data', axis=1)
+        for idx, model_values in enumerate(predictions.iteritems()):
+            axs[idx].plot(self.data, label="Original", color="navy")
+            axs[idx].plot(model_values[1], label="Modelled", color="aqua")
+            axs[idx].set_title(model_values[0] + " Model Fit", size=15, pad=15)
+            axs[idx].set_xticklabels(model_values[1].index, rotation=60)
 
-        return self._save_graph("model-fit.png")
+        plt.legend()
+        self.modelfit = self._save_graph("model-fit.svg")
 
-    def _plot_seanonal_decomposition(self, data):
+    def _plot_seanonal_decomposition(self):
         """
-        Performs basic seasonal decomposition, plots the various components,
-        saves the graphs as a png file, and returns its location.
+        Perform seasonal decomposition with LOESS, plot the various components,
+        and save the graphs as an svg file.
         """
-        # Seasonal decomposition with LOESS
-        sm.tsa.STL(data).fit().plot().autofmt_xdate()
-        return self._save_graph("seasonal-decomp.png")
+        sm.tsa.STL(self.data).fit().plot().autofmt_xdate()
+        self.seasonal_decomposition = self._save_graph("seasonal-decomp.svg")
 
     def _save_graph(self, name):
-        """Names and saves graphs in `self.file_folder`"""
-        filepath = f"{self.file_folder}{str(datetime.now())}_{name}"
+        """Give time-stamped names to matplotlib graphs, and save them."""
+        filepath = f"{self.save_dir}{str(datetime.now())}_{name}"
         plt.savefig(filepath, transparent=True)
         return filepath
 
     def _terminate(self):
-        """Closes all lingering `matplotlib.pyplot` `Figure`s"""
+        """Closes all lingering `matplotlib.pyplot` figures."""
 
         plt.close("all")
