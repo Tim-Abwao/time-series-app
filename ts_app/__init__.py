a5d37d0a8e11329b6d3e52aa447567caddcd97b3
Tim-Abwao <abwaomusungu@gmail.com>
2021-09-02 17:32:17 +0300

    Bump version to 0.4.0

--- ts_app/__init__.py
+++ ts_app/__init__.py
@@ -6,7 +6,7 @@ from waitress import serve
 from ts_app.cli import process_cli_args
 from ts_app.dashboard import server
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 logging.basicConfig(level="INFO")
 
