3c6f7da0118e3e22a9c72f9928ef7e6a1f1a2bd9
Tim-Abwao <abwaomusungu@gmail.com>
2022-08-24 23:38:43 +0300

    Add markdown content as strings rather than files
    This eliminates the I/O overhead of reading markdown files.
    Markdown files allowed convenient editing, especially with linting aids,
    but they are not essential.

--- ts_app/components/modelling.py
+++ ts_app/components/modelling.py
@@ -1,4 +1,3 @@
-from pkgutil import get_data
 from typing import Optional, Tuple
 
 import pandas as pd
@@ -57,9 +56,30 @@ forecast_line_plot = dcc.Loading(
     color="#777",
 )
 
-explanatory_text = dcc.Markdown(
-    get_data("ts_app", "assets/summary.md").decode(), className="guide"
-)
+EXPLANATORY_TEXT = """
+### Guide
+
+Graphs reveal the [trend](/glossary#Trend) in the data, and help assess the
+**goodness of fit**. In general, a good model should reasonably replicate the
+behaviour of the historical data.
+
+Graphs also help discover [seasonal](/glossary#Seasonality) and
+[cyclic patterns](/glossary#Cyclic%20patterns). These usually manifest as
+occasional peaks or troughs.
+
+[Autocorrelation][1] and [Partial-Autocorrelation][2] plots can provide hints
+on a potentially suitable model to start with. [This article][3] describes how.
+Testing for [stationarity][4], and filtering out seasonal & trend effects is
+an essential first step.
+
+[1]: https://en.wikipedia.org/wiki/Autocorrelation
+[2]: https://en.wikipedia.org/wiki/Partial_autocorrelation_function
+[3]: https://en.wikipedia.org/wiki/Box%E2%80%93Jenkins_method#Autocorrelation_\
+and_partial_autocorrelation_plots
+[4]: https://cran.r-project.org/web/packages/TSTutorial/vignettes/Stationary\
+.pdf
+
+"""
 
 footer_buttons = html.Div(
     [
@@ -103,7 +123,7 @@ def generate_layout(input_source: html.Div) -> html.Div:
                     html.Div(
                         [
                             forecast_line_plot,
-                            explanatory_text,
+                            dcc.Markdown(EXPLANATORY_TEXT, className="guide"),
                             footer_buttons,
                         ],
                         className="line-plot-and-summary",
--- ts_app/pages/home_page.py
+++ ts_app/pages/home_page.py
@@ -1,11 +1,30 @@
-from pkgutil import get_data
-
 import dash
 from dash import dcc, html
 
 dash.register_page(__name__, path="/", chapati=777)
 
-HOMEPAGE_TEXT = get_data("ts_app", "assets/home-page.md").decode("utf-8")
+HOMEPAGE_TEXT = """
+# Time Series App
+
+---
+
+A simple application that illustrates basic [time series analysis][ts-wiki]
+concepts. A [glossary](/glossary) of terms is available to help get started.
+
+You can upload a file, or generate sample data, then interactively fit
+[ARIMA][arima] models.
+
+In a nutshell, *Time Series Analysis* purposes to learn and emulate the
+behaviour of data over time. *Time Series Forecasting* then leverages this
+insight to estimate future values.
+
+![An example of time series analysis results, plotted](/assets/ts.svg)
+
+[ts-wiki]: https://en.wikipedia.org/wiki/Time_series
+[arima]: https://en.wikipedia.org/wiki/Autoregressive_integrated_moving_\
+average
+"""
+
 layout = html.Div(
     className="home-page-content",
     children=[
