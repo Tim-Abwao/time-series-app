eabddd3578e3fa4bb954a6a7cac56bb99067d19e
Tim-Abwao <abwaomusungu@gmail.com>
2023-05-03 11:36:54 +0300

    Update plotly.js version

--- ts_app/dash_app.py
+++ ts_app/dash_app.py
@@ -4,7 +4,7 @@ from dash import Dash, html
 app = Dash(
     "ts_app",
     suppress_callback_exceptions=True,
-    external_scripts=["https://cdn.plot.ly/plotly-basic-2.9.0.min.js"],
+    external_scripts=["https://cdn.plot.ly/plotly-2.22.0.min.js"],
     title="Time Series App",
     meta_tags=[
         {
